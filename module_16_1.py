63d600f2bd9b1b04f067b3294d004acc6930cf80
Mojong666 <162918938+Mojong666@users.noreply.github.com>
2024-07-27 22:37:31 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+async def private() -> dict:
+    return {"message": f"Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def user(user_id: int):
+    return (f"Вы вошли как пользователь № {user_id}")
+
+
+@app.get("/user/{first_name}/{last_name}")
+async def info_user(first_name: str, last_name: int) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {first_name}, Возраст: {last_name}"}
