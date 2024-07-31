043c07e9d783cfda4fab523d7204b27f5497cbd9
Mojong666 <162918938+Mojong666@users.noreply.github.com>
2024-07-31 21:36:52 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,23 +1,31 @@
-from fastapi import FastAPI, Path
-# from typing import Annotated
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-users_db = {'1': 'Имя: Example, возраст: 18'}
+users = {'1': 'Имя: Example, возраст: 18'}
 
 @app.get("/users")
-async def users_all(users: str) -> dict:
-    return users_db
+def get_users():
+    return users
+
 @app.post("/user/{username}/{age}")
-async def user_info(username: str, age: int) -> str:
-    current_index = str(int(max(users_db, key=int))+1)
-    users_db[current_index] = username, age
-    return "User <user_id> is registered"
+def add_user(username: str, age: int):
+    new_id = str(max(map(int, users.keys())) + 1)
+    users[new_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {new_id} is registered"
+
 @app.put("/user/{user_id}/{username}/{age}")
-async def user_change(user_id: int, username: str, age: int) -> str:
-    users_db[user_id] = username, age
-    return "The user <user_id> is registered"
+def update_user(user_id: str, username: str, age: int):
+    if user_id in users:
+        users[user_id] = f"Имя: {username}, возраст: {age}"
+        return f"User {user_id} has been updated"
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
 @app.delete("/user/{user_id}")
-async def user_del(user_id: str) -> str:
-    users_db.pop(user_id)
-    return f"Message with {user_id} was deleted."
+def delete_user(user_id: str):
+    if user_id in users:
+        del users[user_id]
+        return f"User {user_id} has been deleted"
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
